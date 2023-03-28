670dffd309da845d6a3c432a905ed30e89c4b162
Simon <code@lible.dk>
2023-03-28 13:07:53 +0200

    Clean up

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-/*
- =========================================================
- * Vue Black Dashboard - v1.1.2
- =========================================================
 
- * Product Page: https://www.creative-tim.com/product/black-dashboard
- * Copyright 2023 Creative Tim (http://www.creative-tim.com)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
